d5a43a1d7c2ee75a79599392736186404fbaf943
ibrahim-mabunda <murilo.mabunda@gmail.com>
2025-04-29 21:24:12 +0200

    first commit

---
+++ src/main/java/me/dio/muslim/MuslimApplication.java
@@ -0,0 +1,13 @@
+package me.dio.muslim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuslimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuslimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/muslim/MuslimApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.muslim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuslimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
