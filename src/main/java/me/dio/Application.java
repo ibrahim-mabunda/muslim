43560447d4f3b744df1f48f3bd24e00f2e20e916
ibrahim-mabunda <murilo.mabunda@gmail.com>
2025-05-06 08:32:45 +0200

    port

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Database URL: " + System.getenv("DATABASE_URL"));
+        System.out.println("Database Host: " + System.getenv("DATABASE_HOST"));
+        System.out.println("Database Port: " + System.getenv("DATABASE_PORT"));
+        System.out.println("Database Name: " + System.getenv("DATABASE_NAME"));
+        System.out.println("Database User: " + System.getenv("DATABASE_USER"));
+        SpringApplication.run(Application.class, args);
+    }
 
 }
